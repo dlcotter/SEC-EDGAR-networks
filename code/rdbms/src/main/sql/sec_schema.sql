37cb5bf7fe674a005a4c63f63a6dc868809206eb
Steve Roggenkamp <roggenkamps@acm.org>
2018-10-28 09:20:05 -0400

    Added sec_schema.sql for PostgreSQL database, and added SECLoadData.java

---
+++ code/rdbms/src/main/sql/sec_schema.sql
@@ -0,0 +1,62 @@
+
+CREATE TYPE type_of_entity  AS ENUM ('issuer','owner');
+CREATE TYPE type_of_contact AS ENUM ('business','mail','other');
+CREATE TYPE type_of_format AS ENUM ('HTML','PDF','TEXT','XML');
+
+CREATE TABLE entities (
+    cik             text,
+    trading_symbol  text,
+    entity_name     text NOT NULL,
+    entity_type     type_of_entity NOT NULL,
+    irsnumber       bigint,
+    sic             text,
+    sic_number      int,
+    state_of_inc    char(2),
+    fiscal_year_end char(4),
+    PRIMARY KEY (cik)
+);
+
+CREATE TABLE contacts (
+    cik             text REFERENCES entities (cik),
+    filing_date     date,
+    contact_type    type_of_contact,
+    street1         text,
+    street2         text,
+    street3         text,
+    city            text,
+    state           text,
+    zip             text,
+    phone           text,
+    PRIMARY KEY (cik,filing_date,contact_type)
+);
+
+CREATE TABLE filings (
+    accession_number text,
+    submission_type  text,
+    document_count   int,
+    filing_date      date,
+    change_date      date,
+    PRIMARY KEY (accession_number)
+);
+
+CREATE TABLE owner_rels (
+    issuer_cik      text REFERENCES entities (cik) NOT NULL,
+    owner_cik       text REFERENCES entities (cik) NOT NULL,
+    filing_date     date NOT NULL,
+    is_director     boolean NOT NULL,
+    is_officer      boolean NOT NULL,
+    is_10_pct_owner boolean NOT NULL,
+    is_other        boolean NOT NULL,
+    officer_title   text,
+    PRIMARY KEY (issuer_cik,owner_cik,filing_date)
+);    
+
+
+CREATE TABLE documents (
+    filing          text REFERENCES filings (accession_number) NOT NULL,
+    sequence        int,
+    type            text,
+    filename        text,
+    format          type_of_format,
+    PRIMARY KEY (filing,sequence)
+);
