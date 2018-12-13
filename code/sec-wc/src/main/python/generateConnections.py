a7feb589874552e63ff4154df24dae25631ce11f
Steve Roggenkamp <roggenkamps@acm.org>
2018-12-12 22:01:43 -0500

    Add comments to the code

--- code/sec-wc/src/main/python/generateConnections.py
+++ code/sec-wc/src/main/python/generateConnections.py
@@ -2,6 +2,10 @@
 Generate connections from the owner_rels table.
 """
 
+#
+# Steve Roggenkamp
+#
+
 from __future__ import print_function
 
 # from io.TextIOBase import *
@@ -10,6 +14,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql import Row
 from pyspark.sql.types import *
 import sys
+
+#
+# selectConncions porcesses a single set of transactions for a given issueCik
+#
 
 def selectConnection(trans_issuer_t):
     issuerCik    = trans_issuer_t[0]  # get the issuerCIK
@@ -31,6 +39,9 @@ def selectConnection(trans_issuer_t):
             connections[t.rptOwnerCik] = set()              # create an entry for this owner
     return returnRDD
 
+#
+# Generate a Row from the owner_rels table
+#
 def genRow(f):
     return Row(issuerCik    = f[1],
                rptOwnerCik  = f[2],
@@ -41,10 +52,13 @@ def genRow(f):
                isOther      = f[7],
                officerTitle = f[8])
 
+#
+# generateConnections - read an owner_rels file and generate the connections based on transactions
+#                       represented in the data
 def generateConnections(spark, inFile):
     sc = spark.sparkContext
 
-    # owner_rels
+    # read owner_rels and generate a table from it
     lines = sc.textFile(inFile)
     fields = lines.map(lambda l: l.split('|'))
     owner_rels = fields.filter( lambda f: f[1]).map(lambda f: genRow(f))
@@ -58,16 +72,23 @@ def generateConnections(spark, inFile):
                                    StructField('officerTitle', StringType(),True)])
     owner_relsTable = spark.createDataFrame( owner_rels, owner_relsSchema )
     owner_relsTable.createOrReplaceTempView("owner_rels")
-#    owner_relsTable.rdd.saveAsTextFile("owner_rels.text")
 
+    # generate a list of transactions from the table using only transactions which
+    # involve rows having isDirector or isOfficer set
+    
     transactions = spark.sql("SELECT issuerCik,filingDate,rptOwnerCik FROM owner_rels WHERE isDirector = 1 OR isOfficer = 1 ORDER BY issuerCik,filingDate,rptOwnerCik" )
+
     # group transactions by issuer, returring an dict with issueCik as key
     txByIssuer  = transactions.rdd.groupBy(lambda t: t.issuerCik)
+
     # iterate over groups of transactions finding connections
     cxs = txByIssuer.flatMap(lambda t: selectConnection(t))
+
     # collect the connections
     connections = cxs.map(lambda c: (c.owner1,c.owner2,c.issuer)).collect()
     return connections
+
+# Main entry point
 
 if __name__ == "__main__":
     # $example on:init_session$
@@ -75,18 +96,23 @@ if __name__ == "__main__":
         .builder \
         .appName("Generate Connections") \
         .getOrCreate()
-    # $example off:init_session$
-    #   .config("spark.some.config.option", "some-value") \
-    
+
+    # argument containing name of owner_rels table
     if len(sys.argv) > 1:
         inFile = sys.argv[1]
     else:
         inFile = "owner_rels.table"
+
+    # argument containing name of output file
     if len(sys.argv) > 2:
         outFile = sys.argv[2]
     else:
         outFile = "connections.table"
+
+    # generate the connections
     connections = generateConnections(spark,inFile)
+
+    # and write them out
     with open("connections.table","w") as ofile:
         csvwriter = writer(ofile, delimiter='|',lineterminator='\n')
         csvwriter.writerows(connections)
--- code/sec-wc/src/main/python/uniqueEntities.py
+++ code/sec-wc/src/main/python/uniqueEntities.py
@@ -6,7 +6,13 @@ number of filings.  This program selects the latest record in the set
 for each entity.
 """
 
-
+# uniqueEntities.py - Eliminate duplicate entities, saving the latest
+#
+# We use this to keep only one record per entity CIK, keeping the latest
+# one we have encountered to reduce multiple records per CIK.
+#
+# Steve Roggenkamp
+#
 from csv import writer
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
@@ -15,10 +21,15 @@ from pyspark.sql.types import *
 def uniqueEntities(spark):
     sc = spark.sparkContext
 
+    # read the table in
     lines = sc.textFile("entities.data")
     fields = lines.map(lambda l: l.split('|'))
     entities = fields.map(lambda f: (f[1],(f[1],f[2],f[3],f[4],f[5],f[6],f[7],f[8],f[9])))
+
+    # group by CIK and select the one with the latest date
     minimumEntities = entities.groupByKey().map(lambda x: (x[0],max(x[1]))).map(lambda x: x[1]).collect()
+
+    # write it out
     with open("entities_unique.table","w") as of:
         csvwriter = writer(of, delimiter="|")
         csvwriter.writerows(minimumEntities)
